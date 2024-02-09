812649b2f426b636835e0d54913d052598fc1ae3
Erik Mosich <erikmosich@gmail.com>
2024-02-09 12:24:38 -0300

    about

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import styles from'./App.module.css'
+import About from './components/About/About.jsx'
 import Hero from './components/Hero/Hero.jsx'
 import Navbar from './components/Navbar/Navbar.jsx'
 
@@ -10,6 +11,7 @@ function App() {
       <div className={styles.App}>
       <Navbar />
       <Hero />
+      <About />
       </div>
     </>
   )
